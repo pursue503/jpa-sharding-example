9567d6a580ff68dc25379b15062d19b51e28844e
pursue503 <pursue503@naver.com>
2022-12-30 16:37:17 +0900

    Add Table.sql

---
+++ db/table.sql
@@ -0,0 +1,213 @@
+create database jpa_shard_1;
+
+create table jpa_shard_1.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+
+create database jpa_shard_2;
+
+create table jpa_shard_2.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+
+create database jpa_shard_3;
+create table jpa_shard_3.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+create database jpa_shard_4;
+create table jpa_shard_4.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+
+create database jpa_shard_5;
+create table jpa_shard_5.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+
+create database jpa_shard_6;
+create table jpa_shard_6.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+
+create database jpa_shard_7;
+create table jpa_shard_7.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+
+create database jpa_shard_8;
+create table jpa_shard_8.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+
+create database jpa_shard_9;
+create table jpa_shard_9.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+
+create database jpa_shard_10;
+create table jpa_shard_10.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+
+create database jpa_shard_11;
+create table jpa_shard_11.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+
+create database jpa_shard_12;
+create table jpa_shard_12.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+
+create database jpa_shard_13;
+create table jpa_shard_13.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+
+create database jpa_shard_14;
+create table jpa_shard_14.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+
+create database jpa_shard_15;
+create table jpa_shard_15.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+
+create database jpa_shard_16;
+create table jpa_shard_16.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+create database jpa_shard_17;
+create table jpa_shard_17.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+create database jpa_shard_18;
+create table jpa_shard_18.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+create database jpa_shard_19;
+create table jpa_shard_19.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+create database jpa_shard_20;
+create table jpa_shard_20.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+create database jpa_shard_21;
+create table jpa_shard_21.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+create database jpa_shard_22;
+create table jpa_shard_22.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+create database jpa_shard_23;
+create table jpa_shard_23.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+create database jpa_shard_24;
+create table jpa_shard_24.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+create database jpa_shard_25;
+create table jpa_shard_25.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+create database jpa_shard_26;
+create table jpa_shard_26.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+create database jpa_shard_27;
+create table jpa_shard_27.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+create database jpa_shard_28;
+create table jpa_shard_28.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+create database jpa_shard_29;
+create table jpa_shard_29.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+
+create database jpa_shard_30;
+create table jpa_shard_30.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+
+
+create database jpa_shard_31;
+create table jpa_shard_31.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+
+create database jpa_shard_32;
+create table jpa_shard_32.product
+(
+    id           int primary key auto_increment not null,
+    product_name varchar(50)                    not null
+);
+
