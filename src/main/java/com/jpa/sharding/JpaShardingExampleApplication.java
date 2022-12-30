c1e675f1197ce71862372352293a5c1cae574d7d
pursue503 <pursue503@naver.com>
2022-12-30 16:10:40 +0900

    Add docker-compose.yml ( db setting )

---
+++ src/main/java/com/jpa/sharding/JpaShardingExampleApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.sharding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaShardingExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaShardingExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jpa/sharding/JpaShardingExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.sharding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaShardingExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
